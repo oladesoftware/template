a91f813a2ea543165f07a09fc40179e0abfd72c3
Helmut <helmut.savoedo@olade.group>
2023-12-06 02:04:44 +0100

    Replace Router by oladesoftware/router and add a 404 file for not found route

---
+++ app/View/404.view.php
@@ -0,0 +1,19 @@
+<?php $title = "Page not found" ?>
+
+<section style="
+    width: 100dvw;
+    height: 100dvh;
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    padding: 20px;
+    text-align: center;
+">
+    <article>
+        <h1 style="margin: 1rem 0; font-size: 2rem;">Oops! Page not found :(</h1>
+        <p style="margin: 0 0 2rem;">It seems that the page you are looking for doesn't exist.</p>
+        <p style="margin: 0.75rem 0;">
+            <a href="/">There are nothing to see here, go back</a></p>
+    </article>
+</section>
--- app/app.php
+++ app/app.php
@@ -1,6 +1,26 @@
 <?php
 
+use App\Core\Http\Request;
+use Oladesoftware\Router\Router;
+
 require(dirname(__DIR__) . "/vendor/autoload.php");
 require(__DIR__ . "/Core/Helper/debug.php");
 
-require(__DIR__ . "/Core/Router/Router.php");
+$router = new Router();
+
+$request = new Request($_SERVER);
+$method = $request->getMethod();
+$path =$request->getUrlPath();
+
+$router->addRoute("GET", "/", function (){
+    require(__DIR__ . "/Controller/indexController.php");
+});
+
+$match = $router->match($path, $method);
+
+if(!$match){
+    http_response_code(404);
+    echo \App\Core\Http\HtmlResponse::render("404", "template");
+}
+
+$router->run($match);
